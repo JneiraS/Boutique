334ba5e70613f1d183669d818f0df2f89fdadb30
NeiraJ <johnny.neira@gmail.com>
2024-09-10 14:36:39 +0200

    refactor: amélioration de l'affichage des catégories et des produits
    - Refactorisation du HTML pour supprimer les sections de catégories statiques.
    - Ajout d'une nouvelle classe `Categorie` dans `script.js` pour générer dynamiquement les catégories à partir des données CSV.
    - Modification de la classe `Product` pour inclure la catégorie dans les propriétés du produit.
    - Mise à jour du système d'insertion des produits en fonction de leurs catégories respectives.
    - Révision du style CSS pour améliorer la présentation des catégories et des produits (alignement, bordures, ombrages, etc.).
    - Amélioration du style des boutons et correction d'une erreur dans les sélecteurs CSS pour les états de survol.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // Fonction pour récupérer les données depuis un fichier CSV
 function getData(csvFilePath) {
   return fetch(csvFilePath)
-    .then((response) => response.text())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erreur lors du chargement du fichier CSV");
+      }
+      return response.text();
+    })
     .then((data) => {
       const rows = data.split("\n");
       const headers = rows[0].split(",");
@@ -16,21 +21,23 @@ function getData(csvFilePath) {
       });
 
       return arrayData;
-    });
+    })
+    .catch((error) => console.error("Erreur :", error));
 }
 // Classe Product pour créer et afficher les produits
 class Product {
-  constructor(nom, image, prix) {
+  constructor(nom, image, prix, categorie) {
     this.nom = nom;
     this.image = image;
     this.prix = prix;
+    this.categorie = categorie;
   }
 
   generateHTML() {
     return `
                   <div class="card">
                     <div class="image_container">
-                        <img src="statics\\${this.image}" alt="${this.nom}">
+                        <img src="statics/${this.image}" alt="${this.nom}">
                     </div>
                     <div class="title">
                         <span>${this.nom}</span>
@@ -55,14 +62,53 @@ class Product {
   }
 }
 
-// Utilisation des données pour créer et afficher des objets Product
+class Categorie {
+  constructor(name) {
+    this.name = name;
+  }
+
+  generateHTML() {
+    return `
+            <section class="${this.name}-wrapper">
+            <p>${this.name}</p>
+            <div class="products-wrapper">
+            </div>
+        </section>
+     `;
+  }
+
+  display() {
+    const categoryWrapper = document.querySelector("main");
+    categoryWrapper.insertAdjacentHTML("beforeend", this.generateHTML());
+  }
+}
+
+// Récupération des catégories depuis le CSV
+const categories = new Set();
+getData("data.csv").then((data) => {
+  data.forEach((productData) => categories.add(productData["Catégorie"]));
+
+  categories.forEach((category) => {
+    const categorie = new Categorie(category);
+    categorie.display();
+  });
+});
+
+// Récupération des produits depuis le CSV
 getData("data.csv").then((data) => {
   data.forEach((productData) => {
     const product = new Product(
       productData["Nom"],
       productData["Image"],
-      productData["Prix"]
+      productData["Prix"],
+      productData["Catégorie"]
     );
-    product.display();
+
+    const categoryWrapper = document.querySelector(
+      `.${product.categorie}-wrapper .products-wrapper`
+    );
+    if (categoryWrapper) {
+      categoryWrapper.insertAdjacentHTML("beforeend", product.generateHTML());
+    }
   });
 });
